521dc2457b64d4951f5cda9041acf2f18c5f8e55
David Derthick <25013744+DDerthickJr@users.noreply.github.com>
2022-02-11 13:22:10 -0500

    Added Copy to Clipboard Function

--- wordle/WordleFrame.java
+++ wordle/WordleFrame.java
@@ -6,6 +6,8 @@ import javax.swing.text.AttributeSet;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.PlainDocument;
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,15 +21,19 @@ public class WordleFrame extends JFrame {
     private JTextField inputField;
     private JButton submitButton;
     private JButton restartButton;
+    private JButton copyButton;
     private int line;
     private String word;
     private List<String> words;
     private List<String> usedWords;
+    private boolean gameWon;
 
     public WordleFrame(){
         initComponents();
         line = 0;
         usedWords = new ArrayList<>();
+        gameWon = false;
+        copyButton.setEnabled(false);
 
         inputField.setDocument(new PlainDocument(){
             @Override
@@ -51,6 +57,10 @@ public class WordleFrame extends JFrame {
         restartButton.addActionListener((event)->{
             restart();
         });
+
+        copyButton.addActionListener((event)->{
+            copyToClipBoard();
+        });
         try{
             words = Files.readAllLines(Path.of("resources/words.txt"));
             word = newWord();
@@ -61,15 +71,17 @@ public class WordleFrame extends JFrame {
     }
 
     private void submit(String input){
-        if(line < 6){
+        if(line < 6 && !gameWon){
             if(words.contains(input) && !usedWords.contains(input)){
                 usedWords.add(input);
                 List<Character> usedLetters = new ArrayList<>();
+                int counter = 0;
                 for(int i = 0; i < input.length(); i++){
                     Character character = input.charAt(i);
                     if(character == word.charAt(i)){
                         insertCharacter(character.toString(), line, i, Color.GREEN);
                         usedLetters.add(character);
+                        counter++;
                     }
                     else if(word.contains(character.toString())){
                         if(!usedLetters.contains(character)){
@@ -89,8 +101,17 @@ public class WordleFrame extends JFrame {
                     }
                 }
                 inputField.setText("");
-                line++;
+                if(counter == 5){
+                    gameWon = true;
+                    copyButton.setEnabled(true);
+                }
+                else{
+                    line++;
+                }
             }
+        }
+        else if(gameWon){
+            copyButton.setEnabled(true);
         }
         else{
             inputField.setText(word);
@@ -106,7 +127,10 @@ public class WordleFrame extends JFrame {
 
     private void restart(){
         word = newWord();
+        System.out.println(word);
         inputField.setText("");
+        copyButton.setEnabled(false);
+        gameWon = false;
         usedWords = new ArrayList<>();
         line = 0;
         for(int i = 0; i < 6; i++){
@@ -123,23 +147,54 @@ public class WordleFrame extends JFrame {
         return words.get(random.nextInt(words.size()));
     }
 
+    private void copyToClipBoard(){
+        StringBuilder message = new StringBuilder();
+        message.append("Wordle ").append(line + 1).append("/6\n\n");
+        for(int i = 0; i < line + 1; i++){
+            for(int j = 0; j < 5; j++){
+                JTextField cell = wordMap[i][j];
+                Color cellColor = cell.getBackground();
+                if(cellColor == Color.GREEN){
+                    message.append("\uD83D\uDFE9");
+                }
+                else if(cellColor == Color.YELLOW){
+                    message.append("\uD83D\uDFE8");
+                }
+                else{
+                    message.append("⬛");
+                }
+            }
+            message.append("\n");
+        }
+        StringSelection selection = new StringSelection(message.toString());
+        Clipboard board = Toolkit.getDefaultToolkit().getSystemClipboard();
+        board.setContents(selection, selection);
+    }
+
     private void initComponents(){
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setVisible(true);
         this.setLayout(new BorderLayout());
+        JPanel controlPanel = new JPanel();
+        controlPanel.setLayout(new BorderLayout());
         JPanel wordPanel = new JPanel();
         wordPanel.setLayout(new GridLayout(6, 5));
-        JPanel controlPanel = new JPanel();
-        controlPanel.setLayout(new GridLayout(0,3));
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new GridLayout(0,3));
+        JPanel clipboardPanel = new JPanel();
+        clipboardPanel.setLayout(new GridLayout(0,1));
         inputField = new JTextField();
         inputField.setFont(inputField.getFont().deriveFont(72F));
         submitButton = new JButton("Submit");
         submitButton.setFont(submitButton.getFont().deriveFont(72F));
         restartButton = new JButton("Restart");
         restartButton.setFont(restartButton.getFont().deriveFont(72F));
-        controlPanel.add(restartButton);
-        controlPanel.add(inputField);
-        controlPanel.add(submitButton);
+        copyButton = new JButton("Copy To Clipboard");
+        copyButton.setFont(copyButton.getFont().deriveFont(72F));
+        clipboardPanel.add(copyButton);
+        inputPanel.add(restartButton);
+        inputPanel.add(inputField);
+        inputPanel.add(submitButton);
         wordMap = new JTextField[6][5];
         for(int i = 0; i < 6; i++){
             for(int j = 0; j < 5; j++){
@@ -154,6 +209,9 @@ public class WordleFrame extends JFrame {
                 wordMap[i][j] = letterField;
             }
         }
+        controlPanel.add(inputPanel, BorderLayout.NORTH);
+        controlPanel.add(clipboardPanel, BorderLayout.SOUTH);
+
         this.add(wordPanel, BorderLayout.CENTER);
         this.add(controlPanel, BorderLayout.SOUTH);
         this.pack();
